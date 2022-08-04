731d32b6e537ac06ce2a9683449521ff8b864bb0
almanza2512 <110559929+almanza2512@users.noreply.github.com>
2022-08-03 20:10:35 -0600

    Update funciones.py

--- Tarea_1/funciones.py
+++ Tarea_1/funciones.py
@@ -1,12 +1,14 @@
 
 
 def basic_operations(Op1, Op2, Op):
+    #la funcion isinstance() verifica que los parametros de entrada sean del typo int()
     if isinstance(Op1, int) & isinstance(Op2, int) & isinstance(Op, int):
         if Op == 1:
             return Op1 + Op2
         elif Op == 2:
             return Op1 - Op2
         elif Op == 3:
+            #verifica que el opreando 2 no sea 0
             if Op2 == 0:
                 return 3312
             else:
